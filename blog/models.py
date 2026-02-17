4400431ba2eb2f536916a446ebbfc2c337063454
jtargo100 <targonsk@hotmail.com>
2026-02-16 20:50:28 -0600

    Add visuals app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Visuals(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.title
